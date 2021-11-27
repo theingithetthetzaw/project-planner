f7cadd7889be66cb415e16ebee740b5d507acfa7
Thone Thone <theingithetthetzaw94@gmail.com>
2021-11-27 13:35:02 +0630

    add form and navbar

---
+++ src/views/AddProject.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>Add Project</h1>
+  <form>
+      <label>Project Title</label>
+      <input type="text">
+      <label>Project Detail</label>
+      <input type="text">
+      <button>Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    form{
+        background-color :white;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    label{
+        display: block;
+        color: #bbb;
+        text-transform: uppercase;
+        font-size: 14px;
+        font-weight: bold;
+        letter-spacing: 1px;
+        margin: 20px 0 10px 0;
+    }
+    input{
+        padding: 10px;
+        border: 0;
+        border-bottom: 1px solid #ddd;
+        width: 100%;
+        box-sizing: border-box;
+    }
+    button{
+        display: block;
+        margin: 20px auto 0;
+        background: #00ce89;
+        color: white;
+        padding: 10px;
+        border: 0;
+        border-radius: 6px;
+        font-size: 16px;
+    }
+</style>
